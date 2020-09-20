09052d8ef9b656fabec2b7b8a21cd2c3f32426b0
Meghanath91 <meghanath.balaji@gmail.com>
2020-09-19 20:56:19 -0400

    built registration page

--- src/app/pages/Login/Login.js
+++ src/app/pages/Login/Login.js
@@ -3,45 +3,43 @@ import { Button, TextField } from "@material-ui/core";
 
 export default function Login() {
   return (
-    <div>
-      <form>
-        <h1>Login</h1>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          name="email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          label="Email Address"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          // className={classes.submit}
-        >
-          Sign In
-        </Button>
-      </form>
-    </div>
+    <form>
+      <h1>Login</h1>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        name="email"
+        // value={email}
+        // onChange={(e) => setEmail(e.target.value)}
+        label="Email Address"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        // value={password}
+        // onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        // className={classes.submit}
+      >
+        Sign In
+      </Button>
+    </form>
   );
 }
---
+++ src/app/pages/Register/Register.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
+
+export default function Login() {
+  return (
+    <form>
+      <h1>Register</h1>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="name"
+        name="Name"
+        // value={email}
+        // onChange={(e) => setEmail(e.target.value)}
+        label="Full name"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        name="email"
+        // value={email}
+        // onChange={(e) => setEmail(e.target.value)}
+        label="Email Address"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        // value={password}
+        // onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        // className={classes.submit}
+      >
+        Sign In
+      </Button>
+    </form>
+  );
+}
